b06aa268d5a9020d223705c63536c033b6482d52
apoorv070 <apoorvpeb1@gmail.com>
2019-10-29 18:58:31 +0530

    third commit:succesfully established the connection with data base

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+//require the library
+const mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb://localhost/todoapp_db');
+
+//acquire the connection to check if it is succesful or not 
+const db = mongoose.connection;
+
+//error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//up and running 
+db.once('open', function() {
+    // we're connected!
+    console.log('connected to the database');
+  });
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var app = express();
-var port = 8000;
+//including the express js 
+const express = require('express');
+
+//connecting to the port
+const port = 8000;
+
+//setting the connection with mongoose
+const db = require('./config/mongoose');
+
+const app = express();
+
 
 //setting up for an view enjine
+
 //isuue 1:what is the diffrence between these 2 lines and bottom most lines giving diffrent error in both the cases 
 //in the uper lines not able to find in the directory
 //app.set('view engine','ejs');
@@ -10,6 +19,10 @@ var port = 8000;
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+//setting up the middleware
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
 //use express router
 app.use('/',require('./routes/index'));
